c9e8324a02e9c78f5f0449d304f7f983e123cd36
Bechev <bertrand.chevalier@gmail.com>
2018-09-14 07:48:02 -0400

    Fixed index API

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,7 @@ class ItemsController < ApplicationController
 
     def index
         @items = Item.all
-        respond_to do |f|
-            f.html
-            f.json {render json: @items, status: 201}
-        end
+        render json: @items, status: 201
     end
     
     def show
